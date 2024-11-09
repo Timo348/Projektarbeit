392216c2a5203540b97476dd0f612601d5169aa9
Timohub3005 <Timohoppe07@gmail.com>
2024-11-09 14:04:31 +0100

    Created files for user creation

---
+++ user/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="">
+        <h2>Login</h2>
+        <label for="username">Geb deinen Nutzernamen ein:</label>
+        <input type="text" required>
+
+        <label for="passwd">Gebe dein Passwort ein:</label>
+        <input type="password" required>
+
+        <button type="submit">Log in</button>
+
+        <label for="noaccount">Noch keinen account?</label>
+        <a href="register.php">Hier geht es zur Registrierung</a>
+    </form>
+</body>
+</html>
