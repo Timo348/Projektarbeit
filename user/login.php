4bc02802d97c2558f80710956d4393fcb5e31af2
Timohub3005 <Timohoppe07@gmail.com>
2024-11-13 19:39:29 +0100

    Started with Login and Register

--- user/login.php
+++ user/login.php
@@ -2,22 +2,37 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
     <form action="">
         <h2>Login</h2>
-        <label for="username">Geb deinen Nutzernamen ein:</label>
-        <input type="text" required>
+        <label for="username">Gib deinen Nutzernamen ein:</label>
+        <input id="usrnm" name="usrnm" type="text" required>
 
-        <label for="passwd">Gebe dein Passwort ein:</label>
-        <input type="password" required>
+        <label for="passwd">Gib dein Passwort ein:</label>
+        <input id="pswd" name="passwd" type="password" required>
 
         <button type="submit">Log in</button>
 
-        <label for="noaccount">Noch keinen account?</label>
+        <label for="noaccount">Noch keinen Account?</label>
         <a href="register.php">Hier geht es zur Registrierung</a>
     </form>
+
+    <?php
+    $username = $_REQUEST['usrnm'] ?? '';
+    $password = $_REQUEST['passwd'] ?? '';
+
+    if ($username == '') {
+        echo"Bitte geben sie ihr Benutzernamen ein";
+    }
+    if ($password == '') {
+        echo"Bitte geben sie ihr Passwort ein";
+    }
+    
+// Funktion um zu Überprüfen ob Login und Passwort Korrekt sind
+// Funktion um eingelogt zu werden
+    
+    ?>
 </body>
 </html>
--- user/register.php
+++ user/register.php
@@ -6,6 +6,29 @@
     <title>Register</title>
 </head>
 <body>
-    
+<form action="">
+        <h2>Registrierung</h2>
+        <label for="username">Gib deinen Nutzernamen ein:</label>
+        <input id="usrnm" name="usrnm" type="text" required>
+
+        <label for="passwd">Gib dein Passwort ein:</label>
+        <input id="pswd" name="passwd" type="password" required>
+
+        <button type="submit">Registerstrierung Abschließen! :D</button>  
+</form>
+<?php
+
+    $username = $_REQUEST['usrnm'] ?? '';
+    $password = $_REQUEST['passwd'] ?? '';
+
+    if ($username == '') {
+        echo"Bitte erstellen sie ein Benutzernamen";
+    }
+    if ($password == '') {
+        echo"Bitte geben sie ein Valides Passwort ein";
+    }
+// Funktion um Daten in die Datenbank einzutragen
+
+?>
 </body>
 </html>
