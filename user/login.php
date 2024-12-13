727b7f0a92ef74908f379df25732fdebed99a2ac
Timohub3005 <Timohoppe07@gmail.com>
2024-12-13 13:49:47 +0100

    ...

--- user/login.php
+++ user/login.php
@@ -15,12 +15,8 @@
 </nav>
     <form class="maincontainer" action="">
         <h2>Log In Seite</h2> 
-        <label for="username">Bitte gib deinen Benutzernamen ein:</label>
         <input id="username" name="username" type="text" placeholder="Benutzername" required> <br>
-
-        <label for="password">Bitte gib dein Passwort ein:</label>
         <input id="passwort" name="passwort" type="password" placeholder="Passwort" required> <br>
-
         <button type="submit">Einloggen</button> <br>
 
         <p>Noch keinen Account?</p>
