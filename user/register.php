25b20cd0d834050662094ae7ac581eacfdc25da6
LuggasLuggasLuggas <gierendlukas@gmail.com>
2024-12-14 18:50:30 +0100

    :D

--- user/register.php
+++ user/register.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" href="../nav.css">
 </head>
 <body>
-<<<<<<< HEAD
-=======
 <nav>
     <a href="../notes/notizen.php">Notizen</a>
     <a href="../todo/todo.php">To-Do</a>
@@ -17,29 +15,16 @@
     <a href="../user/login.php">Login</a>
 </nav>
 <div class="maincontainer">
->>>>>>> 39f19abd82e44640d31d5e439b6a4071455bbaea
 <form action="" method="POST">
         <h2>Registrierung</h2>
         <input id="usrnm" name="usrnm" type="text" placeholder="Benutzernamen" required>
 
-<<<<<<< HEAD
-        <label for="passwd">Gib dein Passwort ein:</label>
-        <input id="passwd" name="passwd" type="password" required>
-=======
         <input id="passwd" name="passwd" type="password" placeholder="Passwort" required>
->>>>>>> 39f19abd82e44640d31d5e439b6a4071455bbaea
 
         <button type="submit">Registerstrierung Abschließen! :D</button>
 </form>
 <?php
 ini_set('display_errors', '1');
-<<<<<<< HEAD
-
-// Verbindung zur SQLite-Datenbank herstellen
-    $db = new PDO('sqlite:C:\xampp\htdocs\Projektarbeit\database\projektdatenbank.db');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-=======
->>>>>>> 39f19abd82e44640d31d5e439b6a4071455bbaea
 
 
     $db = new PDO('sqlite:C:\xampp\htdocs\Projektarbeit\database\projektdatenbank.db');
@@ -56,26 +41,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')  {
         $statement->execute();
 
         echo "Registrierung war erfolgreich! Sie können sich jetzt anmelden.";
-<<<<<<< HEAD
-    } catch(PDOException $e){
-        // Fehler behandeln, z.B. wenn der Benutzername schon existiert
-        if ($e->getCode()=='23000'){
-=======
     } catch(PDOException $e)    {
         // Fehler behandeln, z.B. wenn der Benutztername schon existiert
         if ($e->getCode()=='23000') {
->>>>>>> 39f19abd82e44640d31d5e439b6a4071455bbaea
             echo "Benutzername ist bereits registriert.";
         } 
         else    {
             echo "Ein Fehler ist aufgetreten :/". $e->getMessage();
         }
     }
-<<<<<<< HEAD
-
-=======
 }
->>>>>>> 39f19abd82e44640d31d5e439b6a4071455bbaea
 ?>
 </div>
 </body>
